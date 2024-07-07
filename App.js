8480a60c8938817f55068f369a374565bc1080b3
Ratheesh CN <ratheeshcn87@gmail.com>
2024-07-07 10:13:35 +0400

    changed app.js

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { cloneElement, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGolas, setCourseGoals] = useState([]);
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random.toString },
+    ]);
+  }
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          placeholder="Your course Goal"
+        ></TextInput>
+
+        <Button title="Add Goal" onPress={addGoalHandler}></Button>
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <FlatList
+          data={courseGolas}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goatTex}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          alwaysBounceVertical={false}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +56,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
+  },
+  textInputContainer: {
+    backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    borderColor: "#ccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+    borderWidth: 1,
+  },
+
+  detailsContainer: {
+    flex: 5,
+  },
+
+  goalItem: {
+    borderRadius: 6,
+    padding: 12,
+    margin: 8,
+    backgroundColor: "#5e0acc",
+    color: "white",
+  },
+  goatTex: {
+    color: "white",
   },
 });
